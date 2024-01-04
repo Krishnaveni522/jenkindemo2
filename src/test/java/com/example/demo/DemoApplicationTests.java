bea57f1b503dba7f96b5a0c62e92d2ff52a6ec5f
krishnaveni522 <kondakrishnaveni47@gmail.com>
2024-01-05 02:01:58 +0530

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	public static org.slf4j.Logger log=LoggerFactory.getLogger(DemoApplicationTests.class);
+	
+
+	@Test
+	void contextLoads() {
+		log.info("testing started............");
+		
+		assertEquals(true,true);
+	}
+
+}
